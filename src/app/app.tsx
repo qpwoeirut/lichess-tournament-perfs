67084ef073a66913ea6e82dfedd7276df4aed110
qpwoeirut <stanleyzhong8@gmail.com>
2024-11-14 00:12:24 -0800

    Set my username to default and set a max-width on the app

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -5,14 +5,14 @@ import {fetchTournamentResultsStream} from "@/app/lichess-api";
 import {TournamentResult} from "@/app/types";
 
 export function App() {
-    const [username, setUsername] = useState("");
+    const [username, setUsername] = useState("qpwoeirut");
     const [results, setResults] = useState<TournamentResult[]>([]);
 
     useEffect(() => {
         fetchTournamentResultsStream(username, setResults);
     }, [username]);
 
-    return <main className="container mx-auto my-4">
+    return <main className="max-w-6xl mx-auto p-4">
         { UsernameInput(username, setUsername) }
         { Results(results) }
     </main>
