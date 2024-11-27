96f17b5e50ccda3c7d63e39517df4d796c21831e
qpwoeirut <stanleyzhong8@gmail.com>
2024-11-27 16:58:27 -0600

    Inline unnecessary app.tsx file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
 "use client"
 
-import {App} from "@/app/app";
+import {useEffect, useState} from "react";
+import {Results} from "@/app/results";
+import {UsernameInput} from "@/app/username-input";
+import {fetchTournamentResultsStream} from "@/app/lichess-api";
+import {TournamentResult} from "@/app/types";
 
 export default function Home() {
-  return App();
+  const [username, setUsername] = useState("qpwoeirut");
+  const [results, setResults] = useState<TournamentResult[]>([]);
+
+  useEffect(() => {
+    fetchTournamentResultsStream(username, setResults);
+  }, [username]);
+
+  return <main className="max-w-6xl mx-auto p-4">
+    { UsernameInput(username, setUsername) }
+    { Results(results) }
+  </main>
 }
