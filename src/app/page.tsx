882527bd9a888b31ddd52432765d009bc3ce9761
qpwoeirut <stanleyzhong8@gmail.com>
2024-11-29 23:47:49 -0600

    Add filters to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,36 @@
 "use client"
 
 import {useEffect, useState} from "react";
-import {Results} from "@/app/results";
-import {UsernameInput} from "@/app/username-input";
+import {Description} from "@/app/description";
+import {EMPTY_FILTER_SET, Filters, FilterSet, matchesFilter, TournamentFilter} from "@/app/filters";
 import {fetchTournamentResultsStream} from "@/app/lichess-api";
+import {Results} from "@/app/results";
 import {TournamentResult} from "@/app/types";
+import {UsernameInput} from "@/app/username-input";
 
 export default function Home() {
-  const [username, setUsername] = useState("qpwoeirut");
-  const [results, setResults] = useState<TournamentResult[]>([]);
+    const [username, setUsername] = useState("qpwoeirut");
+    const [allResults, setAllResults] = useState<TournamentResult[]>([]);
+    const [results, setResults] = useState<TournamentResult[]>([]);
+    const [filters, setFilters] = useState<FilterSet>(EMPTY_FILTER_SET);
 
-  useEffect(() => {
-    fetchTournamentResultsStream(username, setResults);
-  }, [username]);
+    useEffect(() => {
+        fetchTournamentResultsStream(username, setAllResults);
+    }, [username]);
+    useEffect(() => {
+        setResults(
+            allResults.filter(result => Object.values(filters).every(
+                (filter: TournamentFilter) => matchesFilter(filter, result)
+            ))
+        );
+    }, [filters, allResults]);
 
-  return <main className="max-w-6xl mx-auto p-4">
-    { UsernameInput(username, setUsername) }
-    { Results(results) }
-  </main>
+    return <main className="max-w-6xl mx-auto p-4">
+        { Description() }
+        <section className="flex">
+            { UsernameInput(username, setUsername) }
+            <Filters filters={filters} setFilters={setFilters} />
+        </section>
+        { Results(results) }
+    </main>
 }
