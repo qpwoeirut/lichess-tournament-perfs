4594883cd545325a28261d4e75cb51cd0606661e
qpwoeirut <stanleyzhong8@gmail.com>
2024-12-01 22:17:13 -0600

    Refactor filtering to use useMemo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import {useEffect, useState} from "react";
+import {useEffect, useMemo, useState} from "react";
 import {Description} from "@/app/description";
 import {EMPTY_FILTER_SET, Filters, FilterSet, matchesFilter, TournamentFilter} from "@/app/filters";
 import {fetchTournamentResultsStream} from "@/app/lichess-api";
@@ -11,28 +11,24 @@ import {UsernameInput} from "@/app/username-input";
 export default function Home() {
     const [username, setUsername] = useState("qpwoeirut");
     const [allResults, setAllResults] = useState<TournamentResult[]>([]);
-    const [results, setResults] = useState<TournamentResult[]>([]);
     const [filters, setFilters] = useState<FilterSet>(EMPTY_FILTER_SET);
+    const filteredResults = useMemo<TournamentResult[]>(
+        () => allResults.filter(result =>
+            Object.values(filters).every((filter: TournamentFilter) => matchesFilter(filter, result))
+        ),
+        [allResults, filters]
+    );
 
-    useEffect(() => {
-        fetchTournamentResultsStream(username, setAllResults);
-    }, [username]);
-    useEffect(() => {
-        setResults(
-            allResults.filter(result => Object.values(filters).every(
-                (filter: TournamentFilter) => matchesFilter(filter, result)
-            ))
-        );
-    }, [filters, allResults]);
+    useEffect(() => fetchTournamentResultsStream(username, setAllResults), [username]);
 
     return <main className="max-w-6xl mx-auto">
         <section className="p-4">
             <Description/>
         </section>
         <section className="p-4">
-            <UsernameInput username={username} setUsername={setUsername} />
-            <Filters filters={filters} setFilters={setFilters} />
+            <UsernameInput username={username} setUsername={setUsername}/>
+            <Filters filters={filters} setFilters={setFilters}/>
         </section>
-        <Results results={results} />
+        <Results results={filteredResults}/>
     </main>
 }
