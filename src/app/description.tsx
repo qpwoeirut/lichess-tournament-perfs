c219513386a1ada21c42ce50b070ea87d22dbf0c
qpwoeirut <stanleyzhong8@gmail.com>
2024-12-01 22:21:06 -0600

    Format files and add newline at end of file

--- src/app/description.tsx
+++ src/app/description.tsx
@@ -6,8 +6,8 @@ export function Description() {
             href="https://lichess.org" target="_blank" rel="noreferrer">Lichess</a>. It was built to help identify
             tournaments which might qualify users for variant titles (such as <a
             href="https://lichess.org/team/horde-variant-titles" target="_blank" rel="noreferrer">
-                Horde Variant Titles
-            </a>).
+            Horde Variant Titles
+        </a>).
         </p>
     </>
 }
