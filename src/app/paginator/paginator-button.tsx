d00064fb53c5a6314ad9cd20bac830757db147f1
qpwoeirut <stanleyzhong8@gmail.com>
2024-12-01 22:45:37 -0600

    Add TODOs for PaginatorButton

--- src/app/paginator/paginator-button.tsx
+++ src/app/paginator/paginator-button.tsx
@@ -11,6 +11,8 @@ const baseClasses = "border border-gray-300 px-4 py-2";
 const buttonColorClasses = "bg-gray-50 hover:bg-gray-100 dark:bg-gray-900 dark:hover:bg-gray-800"
 const selectedClasses = "bg-gray-400 hover:bg-gray-300 dark:bg-gray-600 dark:hover:bg-gray-700"
 
+// TODO: add border rounding for first/last buttons only
+// TODO: add better styles for disabled buttons
 export function PaginatorButton({ selected, children, ...buttonProps }: PaginatorButtonProps) {
     const classes = baseClasses + ' ' + (selected ? selectedClasses : buttonColorClasses);
     return (
