0071f95b89fc5ae2c5649acc0069d611ad98d916
qpwoeirut <stanleyzhong8@gmail.com>
2024-12-03 00:18:11 -0800

    Add Horde norm presets

--- src/app/filters/filter-presets.tsx
+++ src/app/filters/filter-presets.tsx
@@ -1,22 +1,20 @@
 import {FilterOperator, FilterSet, FiltersProps} from "@/app/filters/filters";
-import {DEFAULT_PERFORMANCE_FILTER, EMPTY_FILTER_SET} from "@/app/filters/empty-filters";
+import {
+    DEFAULT_GAMES_PLAYED_FILTER,
+    DEFAULT_PERFORMANCE_FILTER, DEFAULT_RANK_FILTER,
+    DEFAULT_TOTAL_PLAYERS_FILTER, DEFAULT_VARIANT_FILTER,
+    EMPTY_FILTER_SET
+} from "@/app/filters/empty-filters";
 
 const FILTER_PRESETS: Record<string, FilterSet>[] = [
     {
         'None': EMPTY_FILTER_SET
     } as const,
-    {
-        'Horde CM norm': {
-            ...EMPTY_FILTER_SET,
-            performance: {
-                ...DEFAULT_PERFORMANCE_FILTER,
-                operator: FilterOperator.GE,
-                value: 1234,
-            }
-        },
-        'Horde NM norm': EMPTY_FILTER_SET,
-        'Horde IM norm': EMPTY_FILTER_SET,
-        'Horde GM norm': EMPTY_FILTER_SET,
+    { // https://lichess.org/team/horde-variant-titles
+        'Horde CM norm': createHordeFilterPreset({totalPlayers: 8, performance: 2100, gamesPlayed: 7, rank: 5}),
+        'Horde NM norm': createHordeFilterPreset({totalPlayers: 8, performance: 2300, gamesPlayed: 7, rank: 5}),
+        'Horde IM norm': createHordeFilterPreset({totalPlayers: 8, performance: 2500, gamesPlayed: 7, rank: 3}),
+        'Horde GM norm': createHordeFilterPreset({totalPlayers: 8, performance: 2600, gamesPlayed: 7, rank: 1})
     } as const
 ] as const;
 
@@ -33,4 +31,41 @@ export function FilterPresets(props: FiltersProps) {
             )}
         </div>
     );
+}
+
+
+function createHordeFilterPreset({totalPlayers, performance, gamesPlayed, rank}: {
+    totalPlayers: number,
+    performance: number,
+    gamesPlayed: number,
+    rank: number
+}): FilterSet {
+    return {
+        ...EMPTY_FILTER_SET,
+        totalPlayers: {
+            ...DEFAULT_TOTAL_PLAYERS_FILTER,
+            operator: FilterOperator.GE,
+            value: totalPlayers
+        },
+        performance: {
+            ...DEFAULT_PERFORMANCE_FILTER,
+            operator: FilterOperator.GE,
+            value: performance
+        },
+        gamesPlayed: {
+            ...DEFAULT_GAMES_PLAYED_FILTER,
+            operator: FilterOperator.GE,
+            value: gamesPlayed
+        },
+        rank: {
+            ...DEFAULT_RANK_FILTER,
+            operator: FilterOperator.LE,
+            value: rank
+        },
+        variant: {
+            ...DEFAULT_VARIANT_FILTER,
+            operator: FilterOperator.EQ,
+            value: 'Horde'
+        }
+    }
 }
