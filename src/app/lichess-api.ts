9f96fcd95814cc7d0fa2c7e73e4544262b5cd02b
qpwoeirut <stanleyzhong8@gmail.com>
2024-12-06 19:25:49 -0800

    Avoid triggering a re-render for each tournament; german11's tournaments load with only a bit of lag now

--- src/app/lichess-api.ts
+++ src/app/lichess-api.ts
@@ -2,10 +2,14 @@ import type {TournamentResult} from "@/app/types";
 import ndjsonStream, {StreamObject} from "can-ndjson-stream";
 
 
+const BUFFER_SIZE = 50;
+
+
 export function fetchTournamentResultsStream(username: string, setResults: (_: TournamentResult[]) => void) {
     if (username.length === 0) return;
 
     let results: TournamentResult[] = [];
+    let buffered = 0;
 
     fetch(`https://lichess.org/api/user/${username}/tournament/played?performance=true`)
         .then(res => {
@@ -23,9 +27,17 @@ export function fetchTournamentResultsStream(username: string, setResults: (_: T
 
             const reader = stream.getReader();
             const read = (result: StreamObject) => {
-                if (result.done) return;
+                if (result.done) {
+                    setResults(results);
+                    return;
+                }
+
                 results = [...results, result.value as TournamentResult];
-                setResults(results);
+                if (++buffered >= BUFFER_SIZE) {
+                    buffered = 0;
+                    setResults(results);
+                }
+
                 reader.read().then(read);
             }
             reader.read().then(read)
