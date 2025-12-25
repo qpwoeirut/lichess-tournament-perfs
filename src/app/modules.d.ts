e3211c107262c725f55c788b909ef2caa5900111
qpwoeirut <stanleyzhong8@gmail.com>
2025-12-24 22:50:43 -0800

    Simplify can-ndjson-stream typing

--- src/app/modules.d.ts
+++ src/app/modules.d.ts
@@ -1,14 +1,3 @@
 declare module 'can-ndjson-stream' {
-    export default function ndjsonStream(data: unknown): {
-        getReader(): StreamReader;
-    };
-
-    export interface StreamReader {
-        read: () => Promise<StreamObject>
-    }
-
-    export interface StreamObject {
-        done: boolean;
-        value: unknown;
-    }
+    export default function ndjsonStream(data: unknown): ReadableStream
 }
